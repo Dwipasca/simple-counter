6d10b2c3269aff92ee0557cac8ee54594a5568d2
Dwi Pasca Nursanti <dpasca@Dwis-MacBook-Pro.local>
2021-04-16 07:34:13 +0800

    feat: add feature counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
+
+  const handlerInputValue = (e) => {
+    setInputValue(parseInt(e.target.value));
+  };
+
+  const handlerAddCounter = () => {
+    setCounter(counter + inputValue);
+  };
+  const handlerSubtractCounter = () => {
+    setCounter(counter - inputValue);
+  };
+
   return (
     <div className="App">
-      <h1>Hello there!</h1>
+      <h1>Counter</h1>
+      <h1>{counter}</h1>
+      <button onClick={handlerSubtractCounter}>-</button>
+      <input type="number" onChange={handlerInputValue} value={inputValue} />
+      <button onClick={handlerAddCounter}>+</button>
     </div>
   );
 }
