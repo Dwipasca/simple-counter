2288c65ca15095c734fcf19e998284db93ef2fc7
Dwi Pasca Nursanti <dpasca@Dwis-MacBook-Pro.local>
2021-04-16 15:22:55 +0800

    test: clean code App test

--- src/App.test.js
+++ src/App.test.js
@@ -2,33 +2,35 @@ import { render, fireEvent } from "@testing-library/react";
 
 import App from "./App";
 
-// alternatif test
+let getByTestId;
+let titleEl;
+let counterEl;
+let inputEl;
+let subtractBtn;
+let addBtn;
 
-// it("Text Counter is succesfully rendered", () => {
-//   const _expected = "Counter";
-//   const { getByText } = render(<App>{_expected}</App>);
-//   expect(getByText(_expected).toBeInTheDocument);
-// });
+beforeEach(() => {
+  let component = render(<App />);
+  getByTestId = component.getByTestId;
+
+  titleEl = getByTestId("title");
+  counterEl = getByTestId("counter");
+  inputEl = getByTestId("input");
+  subtractBtn = getByTestId("btn-subtract");
+  addBtn = getByTestId("btn-add");
+});
 
 it("Title h1 is succesfully rendered", () => {
-  const { getByTestId } = render(<App />);
-  const titleEl = getByTestId("title");
-
   expect(titleEl.textContent).toBe("Counter");
 });
 
 it("Counter h1 is succesfully rendered", () => {
-  const { getByTestId } = render(<App />);
-  const counterEl = getByTestId("counter");
-
   expect(counterEl.textContent).toBe("0");
 });
 
 it("Input is succesfully rendered and can change value", () => {
-  const { getByTestId } = render(<App />);
-  const inputEl = getByTestId("input");
-
   expect(inputEl.value).toBe("1");
+
   fireEvent.change(inputEl, {
     target: {
       value: "5",
@@ -39,45 +41,26 @@ it("Input is succesfully rendered and can change value", () => {
 });
 
 test("subtract button succesfully render", () => {
-  const { getByTestId } = render(<App />);
-  const subtractBtn = getByTestId("btn-subtract");
-
   expect(subtractBtn.textContent).toBe("-");
 });
 
 test("add button succesfully render", () => {
-  const { getByTestId } = render(<App />);
-  const addBtn = getByTestId("btn-add");
-
   expect(addBtn.textContent).toBe("+");
 });
 
 test("button add  is run correctly", () => {
-  const { getByTestId } = render(<App />);
-  const addBtn = getByTestId("btn-add");
-  const counterEl = getByTestId("counter");
-
   expect(counterEl.textContent).toBe("0");
   fireEvent.click(addBtn);
   expect(counterEl.textContent).toBe("1");
 });
 
 test("button subtract is run correctly", () => {
-  const { getByTestId } = render(<App />);
-  const subtractBtn = getByTestId("btn-subtract");
-  const counterEl = getByTestId("counter");
-
   expect(counterEl.textContent).toBe("0");
   fireEvent.click(subtractBtn);
   expect(counterEl.textContent).toBe("-1");
 });
 
 test("changing input value and clicking on add btn works correctly", () => {
-  const { getByTestId } = render(<App />);
-  const addBtn = getByTestId("btn-add");
-  const counterEl = getByTestId("counter");
-  const inputEl = getByTestId("input");
-
   fireEvent.change(inputEl, {
     target: {
       value: "5",
@@ -88,16 +71,11 @@ test("changing input value and clicking on add btn works correctly", () => {
 });
 
 test("changing input value and clicking on subtract btn works correctly", () => {
-  const { getByTestId } = render(<App />);
-  const subtract = getByTestId("btn-subtract");
-  const counterEl = getByTestId("counter");
-  const inputEl = getByTestId("input");
-
   fireEvent.change(inputEl, {
     target: {
       value: "5",
     },
   });
-  fireEvent.click(subtract);
+  fireEvent.click(subtractBtn);
   expect(counterEl.textContent).toBe("-5");
 });
